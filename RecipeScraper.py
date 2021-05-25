af7cd9ef9a828b6457dc09c840ac23d257719fb6
Philosomniac <shamiton42@yahoo.com>
2021-05-24 20:47:46 -0500

    bs4 arrives

--- RecipeScraper.py
+++ RecipeScraper.py
@@ -1,11 +1,3 @@
+from bs4 import BeautifulSoup
 from urllib.request import urlopen
-
-url = "http://olympus.realpython.org/profiles/aphrodite"
-
-page = urlopen(url)
-
-html_bytes = page.read()
-html = html_bytes.decode("utf-8")
-
-print(html)
 
