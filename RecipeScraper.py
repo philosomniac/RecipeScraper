e96a3989626465258627d817c0a1861b350b9398
Philosomniac <shamiton42@yahoo.com>
2021-05-30 20:57:25 -0500

    pulling list of urls from budgetbytes

--- RecipeScraper.py
+++ RecipeScraper.py
@@ -1,16 +1,25 @@
+from os import link
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
 
-url = "http://olympus.realpython.org/profiles/dionysus"
-page = urlopen(url)
+
+url = "https://www.budgetbytes.com/archive/2010/07"
+req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+page = urlopen(req)
 html = page.read().decode("utf-8")
 soup = BeautifulSoup(html, "html.parser")
 
 # print(soup.get_text())
 
-image1, image2 = soup.find_all("img")
+# image1, image2 = soup.find_all("img")
 
-print(image1["src"])
+# print(image1["src"])
 
+articlelist = soup.find_all("article")
+linklist = []
+for a in articlelist:
+    # print(a)
+    linklist.append(a.find("a"))
 
-
+for l in linklist:
+    print(l.get("href"))
