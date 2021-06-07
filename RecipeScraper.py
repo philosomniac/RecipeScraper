2470e71804ec12e0b428fdd8a387d7a7eaf94de2
Philosomniac <shamiton42@yahoo.com>
2021-06-07 10:08:41 -0500

    img url to class

--- RecipeScraper.py
+++ RecipeScraper.py
@@ -88,7 +88,7 @@ def get_full_recipe_URL_list():
 
 
 class Recipe:
-    def __init__(self, url="", name=None, ingredient_set=None, total_cost=None, serving_cost=None, servings=None, prep_time=None, cook_time=None, instruction_set=None):
+    def __init__(self, url="", name=None, ingredient_set=None, total_cost=None, serving_cost=None, servings=None, prep_time=None, cook_time=None, instruction_set=None, img_url=None):
         self.url = url
         self.name = name
         self.ingredient_set = ingredient_set
@@ -98,6 +98,7 @@ class Recipe:
         self.prep_time = prep_time
         self.cook_time = cook_time
         self.instruction_set = instruction_set
+        self.img_url = img_url
         pass
 
 
