27deb68cfb76204e1594933233a499ed0fe04b2e
Philosomniac <shamiton42@yahoo.com>
2021-08-11 17:18:13 -0500

    again

--- RecipeScraper_test.py
+++ RecipeScraper_test.py
@@ -3,8 +3,12 @@ import RecipeScraper
 
 def test_get_recipe_urls_from_archive_page():
     test_page = "https://www.budgetbytes.com/archive/2010/07/"
-    known_recipe_url = "https://www.budgetbytes.com/chipotle-peach-salsa/"
+    known_recipe_urls = [
+        "https://www.budgetbytes.com/chipotle-peach-salsa/",
+        "https://www.budgetbytes.com/peach-almond-crisp/",
+        "https://www.budgetbytes.com/breakfast-pizza/"
+    ]
 
     urls = RecipeScraper.get_recipe_urls_from_archive_page(test_page)
 
-    assert known_recipe_url in urls
+    assert all(elem in urls for elem in known_recipe_urls)
