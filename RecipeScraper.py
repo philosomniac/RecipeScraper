4a6500f771c93e5083d307542a6539b319ce3ee0
Philosomniac <shamiton42@yahoo.com>
2021-08-11 17:58:44 -0500

    shutting up the linter again

--- RecipeScraper.py
+++ RecipeScraper.py
@@ -270,13 +270,13 @@ def Main():
     setup_logging()
 
     # my_recipes = []
-    recipe_urls = []
-    startline = 3
-    recipe_urls = get_recipe_urls(startline)
+    # recipe_urls = []
+    # startline = 3
+    # recipe_urls = get_recipe_urls(startline)
 
-    for i in range(10):
-        # current_recipe = get_recipe_details_from_url(recipe_urls[i])
-        pass
+    # for i in range(10):
+    #     # current_recipe = get_recipe_details_from_url(recipe_urls[i])
+    #     pass
 
     close_logging()
 
