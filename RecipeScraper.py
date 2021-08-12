de4e5088874a9d0ce0783ab2234463a2b6927815
Philosomniac <shamiton42@yahoo.com>
2021-08-11 21:17:22 -0500

    docstrings to use for refactoring

--- RecipeScraper.py
+++ RecipeScraper.py
@@ -12,6 +12,7 @@ from Models.IngredientSet import IngredientSet
 
 
 def get_recipe_urls_from_archive_page(archiveurl: str) -> list:
+    """Function: Retrieve Recipe List"""
     url_list = []
     try:
         soup = get_parsed_html_from_url(archiveurl)
@@ -25,10 +26,13 @@ def get_recipe_urls_from_archive_page(archiveurl: str) -> list:
 
 
 def get_article_elements_from_page(soup: BeautifulSoup) -> ResultSet:
+    """Function: Retrieve Recipe List"""
+
     return soup.find_all("article")
 
 
 def get_article_urls_from_article_elements(article_elements: ResultSet) -> list:
+    """Function: Retrieve Recipe List"""
     url_list = []
     article_element: PageElement
     for article_element in article_elements:
@@ -42,6 +46,7 @@ def get_article_urls_from_article_elements(article_elements: ResultSet) -> list:
 
 
 def get_parsed_html_from_url(url: str) -> BeautifulSoup:
+    """Function: General scraping"""
     req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
     page = urlopen(req)
     html = page.read().decode("utf-8")
@@ -50,6 +55,7 @@ def get_parsed_html_from_url(url: str) -> BeautifulSoup:
 
 
 def scrape_full_recipe_URL_list(recipe_file_path):
+    """Function: Retrieve Recipe List"""
 
     recipe_url_list = []
 
@@ -73,31 +79,37 @@ def scrape_full_recipe_URL_list(recipe_file_path):
 
 
 def get_archive_page_url_from_date(targetdate: datetime.date) -> str:
+    """Function: Retrieve Recipe List"""
     paddedmonth = str(targetdate.month).zfill(2)
     return "https://www.budgetbytes.com/archive/{0}/{1}/".format(targetdate.year, paddedmonth)
 
 
 def get_recipe_urls(starting_line_index: int) -> list:
+    """Function: Scrape Recipe Details"""
     with open("BudgetBytesRecipes.txt") as recipefile:
         recipe_urls = [line.strip() for line in recipefile]
         return recipe_urls[starting_line_index-1:]
 
 
 def setup_logging():
+    """Function: Application"""
     logging.basicConfig(filename="recipescraper.log",
                         encoding="utf-8", level=logging.INFO, format='%(asctime)s %(message)s')
     logging.info('Started')
 
 
 def close_logging():
+    """Function: Application"""
     logging.info('Finished')
 
 
 def format_price(s: str) -> str:
+    """Function: Scrape Recipe Details"""
     return s.replace("(", "").replace(")", "").replace("$", "")
 
 
 def get_recipe_details_from_url(url: str) -> Recipe:
+    """Function: Scrape Recipe Details"""
     try:
         logging.info(f"getting recipe details from url: {url}")
         soup = get_parsed_html_from_url(url)
@@ -145,6 +157,7 @@ def get_recipe_details_from_url(url: str) -> Recipe:
 
 # TODO: unit test this
 def get_ingredient_from_element(element: PageElement) -> Ingredient:
+    """Function: Scrape Recipe Details"""
     current_amount = get_current_amount(element)
     current_unit = get_current_unit(element)
     if current_unit:
@@ -160,58 +173,72 @@ def get_ingredient_from_element(element: PageElement) -> Ingredient:
 
 
 def get_ingredient_price(i):
+    """Function: Scrape Recipe Details"""
     return i.find(class_="wprm-recipe-ingredient-notes").string
 
 
 def get_ingredient_name(i):
+    """Function: Scrape Recipe Details"""
     return i.find(class_="wprm-recipe-ingredient-name").string
 
 
 def get_current_unit(i):
+    """Function: Scrape Recipe Details"""
     return i.find(class_="wprm-recipe-ingredient-unit")
 
 
 def get_current_amount(i):
+    """Function: Scrape Recipe Details"""
     return i.find(class_="wprm-recipe-ingredient-amount").string
 
 
 def get_ingredient_elements(ingredient_container):
+    """Function: Scrape Recipe Details"""
     return ingredient_container.find_all(class_="wprm-recipe-ingredient")
 
 
 def get_ingredient_container(soup):
+    """Function: Scrape Recipe Details"""
     return soup.find(class_="wprm-recipe-ingredients-container")
 
 
 def get_img_url(soup):
+    """Function: Scrape Recipe Details"""
     return soup.find(class_="wprm-recipe-image").img['data-src']
 
 
 def get_servings_unit(soup):
+    """Function: Scrape Recipe Details"""
     return soup.find(class_="wprm-recipe-servings-unit").string
 
 
 def get_servings(soup):
+    """Function: Scrape Recipe Details"""
     return soup.find(class_="wprm-recipe-servings").string
 
 
 def get_cook_time(soup):
+    """Function: Scrape Recipe Details"""
     return soup.find(class_="wprm-recipe-cook-time-container").get_text().strip()
 
 
 def get_prep_time(soup):
+    """Function: Scrape Recipe Details"""
     return soup.find(class_="wprm-recipe-prep-time-container").get_text().strip()
 
 
 def get_total_time(soup):
+    """Function: Scrape Recipe Details"""
     return soup.find(class_="wprm-recipe-total-time-container").get_text().strip()
 
 
 def get_cost_string(soup):
+    """Function: Scrape Recipe Details"""
     return soup.find(class_="wprm-recipe-recipe_cost").string
 
 
 def get_recipe_title(soup):
+    """Function: Scrape Recipe Details"""
     return soup.find(class_="wprm-recipe-name").string
 
 
