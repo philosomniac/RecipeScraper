8203c5f4627eb23cc921a5963185d204fa68eb49
Philosomniac <shamiton42@yahoo.com>
2021-08-19 21:56:27 -0500

    trying to json

--- Models/Recipe.py
+++ Models/Recipe.py
@@ -1,5 +1,8 @@
+from IngredientSet import IngredientSet
+
+
 class Recipe:
-    def __init__(self, url="", name=None, ingredient_set=None, total_cost=None, serving_cost=None, servings=None, prep_time=None, cook_time=None, instruction_set=None, img_url=None):
+    def __init__(self, url="", name=None, ingredient_set: IngredientSet = IngredientSet([]), total_cost=None, serving_cost=None, servings=None, prep_time=None, cook_time=None, instruction_set=None, img_url=None):
         self.url = url
         self.name = name
         self.ingredient_set = ingredient_set
